a92a07883604545e5a5fced57139559c639e4899
刘赜源 <597944675@qq.com>
2024-10-15 16:37:07 +0800

    add main function

--- Jlinktest.py
+++ Jlinktest.py
@@ -8,25 +8,28 @@
 
 import time
 import pylink
+def main() :
+    jlink = pylink.JLink()
+    while 1 :
+        try:
+            # Initialization
+            
+            jlink.open(0)
+            jlink.connect('Cortex-M3')
 
-jlink = pylink.JLink()
-while 1 :
-    try:
-        # Initialization
-        
-        jlink.open(0)
-        jlink.connect('Cortex-M3')
+            jlink.swo_start(9600)
+            
+            address = 0x00000
 
-        jlink.swo_start(9600)
-        
-        address = 0x00000
+            # Try to read
+            while 1 :
+                num = jlink.memory_read8(address, 1)[0]
+                print("Congratulations! number at address {hex(address)} is : {num}")
+                time.sleep(0.01)
 
-        # Try to read
-        while 1 :
-            num = jlink.memory_read8(address, 1)[0]
-            print("Congratulations! number at address {hex(address)} is : {num}")
-            time.sleep(0.01)
-
-    except pylink.errors.JLinkException as e:
-        print("[JLink Debug Error]: {e}")
-        time.sleep(1)
+        except pylink.errors.JLinkException as e:
+            print("[JLink Debug Error]: {e}")
+            time.sleep(1)
+    
+if __name__ == '__main__':
+    main()
